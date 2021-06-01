c66de8f33a08b047acb02b92db29bdb51356e456
Nikola Mirilo <nikolamirilo@gmail.com>
2021-06-01 20:41:02 +0200

    form page structure changed

--- src/App.js
+++ src/App.js
@@ -93,7 +93,6 @@ class App extends Component {
           render={(props) => (
             <PageWrapper>
               <Form />
-              <Footer {...props} />
             </PageWrapper>
           )}
         />
